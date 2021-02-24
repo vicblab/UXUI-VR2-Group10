f8a0773e0bec44fdd4bd05a9b65b06aa93c192f9
aNaykki <alarik.naykki@edu.turkuamk.fi>
2021-02-24 17:54:27 +0200

    First iteration of working game

---
+++ Assets/GravityManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GravityManager : MonoBehaviour
+{
+    [SerializeField] private List<Rigidbody> gravityObjects = new List<Rigidbody>();
+    [SerializeField] private bool gravityOn;
+    void Start()
+    {
+        foreach (OVRGrabbable grabObj in FindObjectsOfType<OVRGrabbable>())
+        {
+            gravityObjects.Add(grabObj.gameObject.GetComponent<Rigidbody>());
+        }
+    }
+    public void ToggleGravity(bool to)
+    {
+        gravityOn = to;
+        foreach (Rigidbody rb in gravityObjects)
+        {
+            rb.useGravity = gravityOn;
+            rb.isKinematic = !gravityOn;
+        }
+    }
+
+
+}
---
+++ Assets/Timer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public float time = 10f;
+    public float interval = 1f;
+    public TMP_Text timeText;
+    private Coroutine routine;
+
+    public UnityEvent OnTimerStart;
+    public UnityEvent OnTimerEnd;
+    public UnityEvent OnTimerInterval;
+
+
+    public void StartTimer()
+    {
+        if (routine == null)
+        {
+            OnTimerStart?.Invoke();
+            routine = StartCoroutine(TimerRoutine());
+            Debug.Log("StartTimer");
+        }
+    }
+    private IEnumerator TimerRoutine()
+    {
+        float t = time;
+        float it = 0;
+        while (t > 0)
+        {
+            t -= Time.deltaTime;
+            it += Time.deltaTime;
+            if (it >= interval)
+            {
+                it = 0;
+                OnTimerInterval?.Invoke();
+                UpdateTimeText(t);
+            }
+            yield return null;
+        }
+        OnTimerEnd?.Invoke();
+        routine = null;
+
+    }
+    private void UpdateTimeText(float value)
+    {
+        timeText.text = $"{value:F1}s";
+    }
+}
